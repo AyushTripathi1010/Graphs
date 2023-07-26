12c06695a9e01100c7022f4a6b94d54bd12e4435
Ayush Tripathi <74903504+AyushTripathi1010@users.noreply.github.com>
2023-07-26 12:37:04 +0530

    finding cycle using bfs

--- Detect_cycle_in_an_undirected_graph.cpp
+++ Detect_cycle_in_an_undirected_graph.cpp
@@ -1,41 +1,54 @@
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    // 4-2-1
-    //   |
-    //   3
-    bool dfs(int node, vector<int> &vis , vector<int> adj[], int parent)
+    
+    bool bfs(int node , vector<bool> &visited , vector<int> adj[])
     {
-        vis[node] = 1;
+        visited[node]= true;
         
-        for(auto elem : adj[node])
+        queue<pair<int,int>> q;
+        q.push({node , -1}); // 0,-1
+        
+        while(!q.empty())
         {
-            if(!vis[elem])
+            int curr = q.front().first;     // 1
+            int parent = q.front().second; // 0
+            
+            q.pop();
+            for(auto elem : adj[curr])        // 1-> 2 , 4
             {
-                if(dfs(elem , vis , adj, node)) return true;
-            }
-            else 
-            {
-                if(elem != parent) return true;   
                 
+                if(!visited[elem])
+                {
+                    visited[elem] = true;
+                    q.push({elem , curr});   
+                }
+                else
+                {
+                    if(parent != elem)
+                    return true;
+                }
             }
-          
         }
+        
         return false;
     }
+    
+    
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
         
-        vector<int> vis(V, 0);
+        vector<bool> visited(V,false);
         
-        for(int i =0 ; i<V; i++)
+        for(int i =0 ; i<V ; i++)
         {
-            if(!vis[i])
+            if(!visited[i])
             {
-                if(dfs(i , vis , adj, -1)) return true;
+                
+                if(dfs(i, visited , adj ,-1)) return true;
             }
+            
         }
-        
         return false;
     }
 };
